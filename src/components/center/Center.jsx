9d80775a892d073d080397539deaf7ceb57c2e2c
jhondeser <jhonatan.mog2829@gmail.com>
2023-04-19 23:50:21 +0200

    termino seccion carrusel widgets

--- src/components/center/Center.jsx
+++ src/components/center/Center.jsx
@@ -1,11 +1,13 @@
 import SeveralImg from './products/several_img/Several_img'
-import Banner from './products/banner/Banner'
+import Banner from './products/banner/Banner';
+import Witgets from './products/witgets/Witgets';
 
 function Center() {
   return(
     <section className="center">
       <SeveralImg></SeveralImg>
       <Banner></Banner>
+      <Witgets></Witgets>
     </section>
   )
 }
--- src/components/center/products/witgets/witgets.jsx
+++ src/components/center/products/witgets/witgets.jsx
@@ -1,43 +1,73 @@
-import { useEffect, useState } from "react"
+import { useRef, useState } from "react"
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-
+import bose_one from '../../../../assets/acton-iii-black-default.jpg'
+import bose_one__hover from '../../../../assets/acton-iii-black-hover.jpg'
+import bose_two from '../../../../assets/stanmore-iii-black-default.jpg'
+import bose_two__hover from '../../../../assets/stanmore-iii-black-hover.jpg'
+import bose_three from '../../../../assets/woburn-iii-black-default.jpg'
+import bose_three__hover from '../../../../assets/woburn-iii-black-hover.jpg'
+import './witgets.css'
 
 function Witgets() {
+  const [selectedBose,setSelectedBose] = useState(null);
   const bose_list = [
     {
-      imgOne: '',
-      imgTow: '',
-      title: 'ACTON II',
-      price: 0
+      imgOne: bose_one,
+      imgTow: bose_one__hover,
+      title: 'ACTON III',
+      price: 299
     },
     {
-      imgOne: '',
-      imgTow: '',
+      imgOne: bose_two,
+      imgTow: bose_two__hover,
       title: 'STANMORE III',
-      price: 0
+      price: 399
     },
     {
-      imgOne: '',
-      imgTow: '',
+      imgOne: bose_three,
+      imgTow: bose_three__hover,
       title: 'WOBURN III',
-      price: 0
+      price: 599
     }
   ]
+
+  const sliderRef = useRef(null);
+  const targetSlide = 3;
 
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true
   };
 
   return(
     <section className="Witgets">
-
+      <div className="container_witgets">
+        <h2>NUESTROS FAVORITOS</h2>
+        <Slider {...settings} ref={sliderRef}>
+          {bose_list.map(el => 
+            <div 
+              className={selectedCard === movie ? 'selected' : ''}
+              onMouseEnter={() => setSelectedBose(true)}
+              onMouseLeave={() => setSelectedBose(false)}
+            >
+              <div className="bose_container__img">
+                <img src={selectedBose ? el.imgTow : el.imgOne} alt="" />
+              </div>
+              <div className="bose_container__body">
+                <p>{el.title}</p>
+                {selectedBose &&(
+                  <p>€ {el.price}</p>
+                )}
+              </div>
+            </div>
+          )}
+        </Slider>
+      </div> 
     </section>
   )
 }
